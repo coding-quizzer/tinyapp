6e9b364b6659de28c34511858d23228b8fd7a34c
William Gadd <wgadd@sasktel.net>
2022-10-03 23:51:00 +0000

    Implimented post command for the /urls path

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://google.com"
 };
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,6 +32,11 @@ app.get("/urls/:id", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
