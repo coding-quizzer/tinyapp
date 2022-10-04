7b34fa045ddeca6192ff34778b9e39d02df1c413
William Gadd <wgadd@sasktel.net>
2022-10-04 21:49:42 +0000

    Added username to templateVals in express_server.js every time a new page is rendered

--- express_server.js
+++ express_server.js
@@ -23,16 +23,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { 
+      username: req.cookies["username"],
+      id: req.params.id,
+      longURL: urlDatabase[req.params.id]
+    };
   res.render("urls_show.ejs", templateVars);
 });
 
