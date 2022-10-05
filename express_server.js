ed4382b41c98002d999116b57f4c5b05e6c9594d
William Gadd <wgadd@sasktel.net>
2022-10-05 02:57:06 +0000

    Fixed linting errors

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const urlDatabase = {
 
 const generateRandomString = function(len) {
   // generates a random number between 0 and (36 ^ len) and converts it to a string, rendering it base 36
-  return Math.floor(Math.random()*Math.pow(36, len)).toString(36);
+  return Math.floor(Math.random() * Math.pow(36, len)).toString(36);
 };
 
 generateRandomString();
@@ -35,14 +35,14 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["username"]};
   res.render("urls_new", templateVars);
-})
+});
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
-      username: req.cookies["username"],
-      id: req.params.id,
-      longURL: urlDatabase[req.params.id]
-    };
+  const templateVars = {
+    username: req.cookies["username"],
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
   res.render("urls_show.ejs", templateVars);
 });
 
@@ -54,38 +54,38 @@ app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
   res.redirect(longURL);
-})
+});
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(6);
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
 app.post("/urls/:id", (req, res) => {
   const editLongURL = req.body.editLongURL;
   const id = req.params.id;
   urlDatabase[id] = editLongURL;
   res.redirect(`/urls/${id}`);
-})
+});
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
-})
+});
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
-})
+});
 
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
   res.redirect("/urls");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
