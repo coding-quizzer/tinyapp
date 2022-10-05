1ce1846e0c5e1277decc808c948fb24d1c69faf3
William Gadd <wgadd@sasktel.net>
2022-10-05 16:50:49 +0000

    Added new users database to store all the use id

--- express_server.js
+++ express_server.js
@@ -6,6 +6,19 @@ const PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs");
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey=dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://google.com"
