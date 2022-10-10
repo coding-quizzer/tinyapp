8d0b1016cc801ed7af2f101670d37a4905b62259
William Gadd <wgadd@sasktel.net>
2022-10-10 14:11:33 +0000

    Moved generateRandomString and urlsForUser to helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,8 @@
+const generateRandomString = function(len) {
+  // generates a random number between 0 and (36 ^ len) and converts it to a string, rendering it base 36
+  return Math.floor(Math.random() * Math.pow(36, len)).toString(36);
+};
+
 const findUserWithEmail = function(userDatabase, email) {
   for (let userKey in userDatabase) {
     const user = userDatabase[userKey];
@@ -8,4 +13,17 @@ const findUserWithEmail = function(userDatabase, email) {
   return null;
 }
 
-module.exports = { findUserWithEmail };
+const urlsForUser = function(urlDatabase, id) {
+  let urlKeys = Object.keys(urlDatabase);
+  // filter the keys
+  const validKeys = urlKeys.filter(urlKey => urlDatabase[urlKey].userID === id);
+  const validURLs = validKeys.map(validKey => {
+    const url = urlDatabase[validKey];
+    url.id = validKey;
+    return url;
+  });
+
+  return validURLs;
+};
+
+module.exports = { findUserWithEmail, urlsForUser, generateRandomString};
